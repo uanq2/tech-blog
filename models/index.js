5276289f94420e7df5a3f71ab891fdd6b8896940
Uriel Nunez <uanq22@hotmail.com>
2021-07-11 01:05:52 -0500

    Created schema file to crete db in mySQL and created models

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+module.exports = { User, Post, Comment };
